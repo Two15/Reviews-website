aea2da33c9f04d1be5b59d47e2aa3ce663a46118
Xavier Cambar <xcambar@gmail.com>
2016-07-01 17:28:18 +0200

    Adds Sass and Bourbon

--- Brocfile.js
+++ Brocfile.js
@@ -1,1 +1,6 @@
-module.exports = 'public';
+var SassCompiler = require('broccoli-sass');
+var mergeTrees = require('broccoli-merge-trees');
+
+var sassCompiler = new SassCompiler(['src/sass'], 'main.scss', 'main.css');
+
+module.exports = mergeTrees(['public', sassCompiler]);
