e6712ef4c7d577705307620658d327d5deda5432
Xavier Cambar <xcambar@gmail.com>
2016-07-20 02:22:53 +0200

    Builds with s3/Cloudfront in mind

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,27 @@
-var SassCompiler = require('broccoli-sass');
+/*jshint node:true */
+"use strict";
+
+// Modules
+var path = require('path');
+var util = require('util');
+var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
+var replaceFiles = require('broccoli-replace');
+var SassCompiler = require('broccoli-sass');
+
+// values
+var uuid = require('node-uuid')();
+
+var appendRevision = function(tree) {
+  return new Funnel(tree, {
+    getDestinationPath: function(p) {
+      var dir = path.dirname(p);
+      var ext = path.extname(p);
+      var base = path.basename(p, ext);
+      return util.format("%s/%s-%s%s", dir, base, uuid, ext);
+    }
+  });
+};
 
 var sassCompiler = new SassCompiler(
   ['src/sass']
@@ -9,4 +31,36 @@ var sassCompiler = new SassCompiler(
   'main.css'
 );
 
-module.exports = mergeTrees([ 'public', sassCompiler ]);
+var replaceReferences = function(tree) {
+  return replaceFiles(tree, {
+    files: ['index.html'],
+    patterns: [
+      { match: 'revision', replacement: uuid },
+      { match: 'build_time', replacement: (new Date()).toUTCString() }
+    ]
+  });
+};
+
+var copyToStandardIndex = function(tree) {
+  return new Funnel(tree, {
+    include: ['index-' + uuid + '.html'],
+    getDestinationPath: function() { return 'index.html'; }
+  });
+};
+
+var treePromise = Promise.resolve('public')
+  .then(replaceReferences)
+  .then(function(tree) {
+    return mergeTrees([ tree, sassCompiler ]);
+  })
+  .then(appendRevision)
+  .then(function(tree) {
+    return mergeTrees([tree, copyToStandardIndex(tree)]);
+  });
+
+module.exports = {
+  read: function(readTree) {
+    return treePromise.then(readTree);
+  },
+  cleanup: function() {}
+};
